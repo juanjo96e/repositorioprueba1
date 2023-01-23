972911dd356a4f9d07874a55ff2d6842b09d473b
juanjo96e <juanjoseoconalba@hotmail.com>
2023-01-23 14:16:32 +0000

    se sube desde scripts

--- scriptgithub.sh
+++ scriptgithub.sh
@@ -1,5 +1,6 @@
+
 #!/bin/bash
-find ~ -name "*.txt"  -exec tar rvf archivocomprimido.tar {}\;
+find ~ -name "*.txt"  -exec tar rvf archivocomprimido.tar {} \;
 git add .
 git  commit -m  "se sube desde scripts"
 push
